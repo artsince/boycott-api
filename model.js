d88f05029adb10687e895ab60da8e209204330eb
ahmet kizilay <ahmet.kizilay@gmail.com>
2013-07-22 00:04:14 +0300

    added toClient method for opinionModel, changed text to opinion for opinionSchema

--- model.js
+++ model.js
@@ -6,7 +6,7 @@ var apimodel = function () {
     var opinionSchema = new Schema({
         boycott_id: {type: String, required: true},
         boycott_type: {type: String, enum: ['venue', 'corporation', 'event'], required: true},
-        text: {type: String, required: true},
+        opinion: {type: String, required: true},
         agree_count: {type: Number, min: 0, default: 0},
         disagree_count: {type: Number, min: 0, default: 0}
     });
@@ -20,8 +20,19 @@ var apimodel = function () {
         veto_count: {type: Number, min: 0, default: 0}
     });
 
-        // Duplicate the ID field.
+    // deleting auto-generated _id and __v keys
     venueSchema.method('toClient', function() {
+        var obj = this.toObject();
+
+        obj.id = obj._id;
+        delete obj._id;
+        delete obj.__v;
+
+        return obj;
+    });
+
+    // deleting auto-generated _id and __v keys
+    opinionSchema.method('toClient', function() {
         var obj = this.toObject();
 
         obj.id = obj._id;
