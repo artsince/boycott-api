dca922aec7b30cd6e275f2ec0e1d20ea7316b18b
ahmet kizilay <ahmet.kizilay@gmail.com>
2013-07-23 00:21:39 +0300

    indexed to be unchanged forever

--- config/config.heroku.js
+++ config/config.heroku.js
@@ -1,11 +1,18 @@
 /*
  * See config/index.js for further explanation.
  * heroku config overrides global config values
+ * this file is indexed to be assumed unchanged from now on
  */
 
 var config = require('./config.global');
 
 config.env = 'heroku';
+config.mongo.uri = '';
 config.mongo.db = '';
+config.mongo.user = '';
+config.mongo.pass = '';
+config.mongo.port = 10010;
+
+config.mongo.connectionString = 'mongodb://' + config.mongo.user + ':' + config.mongo.pass + '@' + config.mongo.uri + ':' + config.mongo.port + '/' + config.mongo.db;
 
 module.exports = config;
